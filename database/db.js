7c15659b1b3b6a8ec4d3910dbb89e490442fe588
unknown <koushikmondal413@gmail.com>
2025-03-01 14:23:01 +0530

    db setup

--- database/db.js
+++ database/db.js
@@ -1,2 +1,16 @@
-let mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const mongoURI = "mongodb://localhost:27017";
+
+const initializeDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(mongoURI);
+    if (connection) {
+      console.log("Connected Successfully");
+    }
+  } catch (error) {
+    console.log("Connection Failed", error);
+  }
+};
+
+module.exports = { initializeDatabase };
--- middlewares/User.controller.js
+++ middlewares/User.controller.js
@@ -1,3 +1,5 @@
+import { User } from "../models/User.model";
+
 export const signup = () => {
 
 }
@@ -5,20 +7,24 @@ export const signup = () => {
 export const login = () => {
 
 }
+
+export const getAll = () => {
+    const users = User.find()
+    return users
+}
 export const verifyToken = (req, res, next) => {
-  // get the auto token for the headder
     const token = req.headers['authorization'];
   
     if (!token) {
       // if token is not present return the responce with 403
       return res.status(403).json({ message: 'No token provided' });
     }
-  // Verify JWT token
+  
     jwt.verify(token, SECRET_KEY, (err, decoded) => {
       if (err) {
         return res.status(500).json({ message: 'Failed to authenticate token' });
       }
-  
+      
       req.user = decoded;
       next();
     });
