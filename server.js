61b7f35d5e774cf8a28b8c57b553021d91eca9cc
unknown <koushikmondal413@gmail.com>
2025-03-01 15:11:45 +0530

    helath

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express()
 
 app.use('/user', verifyToken, userRoute)
 
+app.get('/healthcheck', (req, res) => {
+    res.send("OK")
+})
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
